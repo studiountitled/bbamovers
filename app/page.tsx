b6ff55af53c4c77678e88429045ac3aaadc3f7e8
Praise Omogbemeh <onosithena@gmail.com>
2026-03-12 22:38:53 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -91,11 +91,11 @@ const reviews = [
 ];
 
 const galleryImages = [
-  "/gallery-1.jpg",
-  "/gallery-2.jpg",
-  "/gallery-3.jpg",
-  "/gallery-4.jpg",
   "/gallery-5.jpg",
+  "/gallery-10.jpg",
+  "/gallery-8.jpg",
+  "/gallery-2.jpg",
+  "/gallery-1.jpg",
 ];
 
 const faqs = [
