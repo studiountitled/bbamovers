cbe3898a42c15156dc9e662beaa482fda4ba88b2
Praise Omogbemeh <onosithena@gmail.com>
2026-03-12 22:23:47 -0500

    fix images

--- app/page.tsx
+++ app/page.tsx
@@ -92,10 +92,10 @@ const reviews = [
 
 const galleryImages = [
   "/gallery-5.jpg",
-  "/gallery-6.jpg",
-  "/gallery-7.jpg",
-  "/gallery-8.jpg",
-  "/gallery-9.jpg",
+  "/gallery-1.jpg",
+  "/gallery-2.jpg",
+  "/gallery-10.jpg",
+  "/gallery-3.jpg",
 ];
 
 const faqs = [
